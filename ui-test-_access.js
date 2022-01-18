7f3ca79f39a0dbb797630c1a8ce07c5dc6ac28c9
Rosano Coutinho <pub@xeari.com>
2022-01-18 12:01:11 -0500

    Lint

--- ui-test-_access.js
+++ ui-test-_access.js
@@ -11,7 +11,7 @@ Object.entries({
 
 	ROCOForumButton: '.ROCOForumButton',
 }).map(function (e) {
-	return global[e.shift()]  = e.pop();
+	return global[e.shift()] = e.pop();
 });
 
 describe('ROCOForum_Access', function () {
